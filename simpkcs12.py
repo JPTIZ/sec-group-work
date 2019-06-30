28105383a5341119fd2bf60bd9707754653e8013
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2019-06-30 15:33:41 -0300

    Minimal work is done.

--- simpkcs12.py
+++ simpkcs12.py
@@ -7,32 +7,73 @@ from carl import command, REQUIRED
 from OpenSSL import crypto
 
 
+class Defaults:
+    ENCODING = 'utf8'
+    DIGEST = 'sha256'
+
+
 @command
 def main(subcommand = REQUIRED):
     pass
 
 
-def load_pkcs12(filename: Path, password: str) -> crypto.PKCS12:
-    '''Loads file containing PKCS12 keys.'''
+def load_pkcs12(
+        filename: Path,
+        password: str,
+        encoding=Defaults.ENCODING
+) -> crypto.PKCS12:
+    '''Loads file containing PKCS12 keys.
+
+    encoding (str): Password encoding.'''
     with open(filename, 'rb') as f:
-        return crypto.load_pkcs12(f.read(), password.encode('utf8'))
+        return crypto.load_pkcs12(f.read(), password.encode(encoding))
 
 
 @main.subcommand
-def sign(document: Path, certificate: Path, password: str):
-    '''Signs document, generating a file with "-signed" suffix.'''
-    # TODO
+def sign(
+        document: Path,
+        certificate: Path,
+        password: str,
+        encoding=Defaults.ENCODING,
+        digest=Defaults.DIGEST,
+):
+    '''Signs document, generating ".sig" suffix.'''
     p12 = load_pkcs12(certificate, password)
 
-    print(p12.get_certificate())
-    print(p12.get_privatekey())
-    print(p12.get_ca_certificates())
+    with open(document, 'r') as f:
+        contents = f.read().encode(encoding)
+
+        sig = crypto.sign(p12.get_privatekey(), contents, digest)
+
+    with open(f'{document.stem}.sig', 'wb') as f:
+        f.write(sig)
 
 
 @main.subcommand
-def verify(document: Path, certificate: Path, password: str):
+def verify(
+        document: Path,
+        signature: Path,
+        certificate: Path,
+        password: str,
+        encoding=Defaults.ENCODING,
+        digest=Defaults.DIGEST,
+):
     '''Checks if file signature is valid.'''
-    pass
+    p12 = load_pkcs12(certificate, password)
+
+    with open(signature, 'rb') as f:
+        sig = f.read()
+
+    with open(document) as f:
+        data = f.read().encode(encoding)
+
+    try:
+        crypto.verify(p12.get_certificate(), sig, data, digest)
+    except:
+        print('Wrong signature.')
+        raise
+
+    print('Valid signature')
 
 
 if __name__ == '__main__':
